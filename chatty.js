823fb832d64a2f5baa6d7510df8683b7fef237f0
Jason Stiltner <jasonstiltner@gmail.com>
2015-10-19 15:37:25 -0500

    javascript works

--- chatty.js
+++ chatty.js
@@ -1,42 +1,28 @@
-
-
 function moveIntoBox (clickEvent  ){
-  var fieldInput = document.getElementById("field-input").value;
-  console.log(fieldInput);
-  var textBox = document.getElementById("text-box");
-  console.log(textBox);
-  // var oldtextbox = textBox;
-  textBox.innerHTML = "<p class ='entry'>" + fieldInput + "<button class ='button' id ='deletor' type='button' target = 'delete' text='Delete'>Delete</button></p><br>" + textBox.innerHTML;  
-  console.log(textBox);
   
+  var fieldInput = document.getElementById("field-input").value;
+  var content = document.getElementById("text-box");
+  var textBox = "<p class ='entry'>" + fieldInput + "<button class ='button' id ='deletor' type='button' target = 'delete' text='Delete'>Delete</button></p><br>";  
+    content.innerHTML = textBox + content.innerHTML;
 }
-  // document.querySelector("body").addEventListener("click", function(event)
-  // {console.log(event);
-    // if (event.target.value === "delete") {
-    // textBox.innerHTML = oldtextbox;
-    // }
-    // else {
-    //   // textBox.innerHTML = textBox;
-    // }
-  // });
-// }
+function deleteIt () {
+if (event.target.id === "deletor") {
+  var checker = event.target.parentNode;
+  console.log(checker);
+  event.target.parentNode.remove();
+    }
+  }
+
+function clearAllText(){
+content.innerHTML = "";  
+}
 
 
 var triggerMove = document.getElementById("submit-input");
 triggerMove.addEventListener("click", moveIntoBox);
 
-
-  // // Handle the click event on any li
-  // if (event.target.tagName.toLowerCase() === "li") {
-  //   console.log("You clicked on an <li> element");
-  // }
-
-  // // Handle the click event on any DOM element with a certain class
-  // if (event.target.className === "article-section") {
-  //   console.log("You clicked on an `article-section` element");
-  // }
-
-  // // Inspect the `id` property of the event target
-  // if (event.target.id === "page-title") {
-  //   console.log("You clicked on the page-title element");
-  // }
+var clearEntry = document.querySelector("body");
+clearEntry.addEventListener("click", deleteIt);
+ 
+var triggerMove = document.getElementById("clear-text");
+triggerMove.addEventListener("click", clearAllText);
