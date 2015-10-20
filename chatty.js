1cae3d26a65592edd8230016c6a8e3a2fdb1bd8f
micahp0506 <micahp0506@gmail.com>
2015-10-20 10:20:19 -0500

    Added bonus features

--- chatty.js
+++ chatty.js
@@ -40,14 +40,33 @@ document.addEventListener("keyup", function (e) {
       }
 });
 
+var numberEntries = [];
+
+function noMoreText () {
+if (numberEntries.length > 20) {
+	console.log(numberEntries.length);
+	document.getElementById("submit-input").disabled = true;
+	document.getElementById("submit-input").classList.add("disable");
+	}
+}
 
 //moves text from input to display box
 function moveIntoBox (clickEvent) {
-  
+  var theDate = new Date();
+  var theMonth = theDate.getMonth();
+  var theDay = theDate.getDate();
+  var theYear = theDate.getFullYear();
+  var theHour = theDate.getHours();
+  var theMinute = theDate.getMinutes();
+  console.log(theYear);
+  var timeStamp = " (" + (theMonth + 1) + "/" + theDay + "/" + theYear  + ", " + theHour + ":" + theMinute + ")";
   var field = document.getElementById("field-input");
   var fieldInput = document.getElementById("field-input").value;
   var content = document.getElementById("text-box");
-  var textBox = "<p class ='entry'>" + fieldInput + "<button class ='button' id ='deletor' type='button' target = 'delete' text='Delete'>Delete</button></p>";  
+  var textBox = "<p class ='entry'>" + fieldInput + timeStamp + "<button class ='button' id ='deletor' type='button' target = 'delete' text='Delete'>Delete</button></p>";  
+  numberEntries.push(textBox);
+  console.log("function next");
+  noMoreText();
   content.innerHTML = textBox + content.innerHTML;
   console.log(fieldInput);
   field.value = " ";
@@ -60,7 +79,11 @@ if (event.target.id === "deletor") {
   var checker = event.target.parentNode;
   console.log(checker);
   event.target.parentNode.remove();
-    }
+  numberEntries.pop();
+  document.getElementById("submit-input").disabled = false;
+  document.getElementById("submit-input").classList.remove("disable");
+    } 
+    noMoreText();
   }
 
 function clearAllText(){
@@ -77,4 +100,6 @@ clearEntry.addEventListener("click", deleteIt);
  
 var triggerMove = document.getElementById("clear-box");
 triggerMove.addEventListener("click", clearAllText);
+
+
 
